70b39d06bd80cd30b30fa6763ac10f34e387ee81
Melissa Kaulfuss <melissacor3lfuss@gmail.com>
2016-11-04 11:48:37 +1100

    Refactor player specs

--- player_spec.rb
+++ player_spec.rb
@@ -1,44 +1,37 @@
 require_relative 'player'
 
 describe Player do 
-  it "has a capitalized name" do
-    player = Player.new("larry", 150)
 
-    expect(player.name).to eq("Larry")
+  before do
+    @initial_health = 150
+    @player = Player.new("larry", @initial_health)
+    $stdout = StringIO.new
   end
 
-  it "has an initial health" do
-    player = Player.new("larry", 150)
+    it "has a capitalized name" do
+      expect(@player.name).to eq("Larry")
+    end
 
-    expect(player.health).to eq(150)
-  end
+    it "has an initial health" do
+      expect(@player.health).to eq(150)
+    end
 
-  it "has a string representation" do
-    player = Player.new("larry", 150)
+    it "has a string representation" do
+      expect(@player.to_s).to eq("I'm Larry with a health of 150 and a score of 155.")
+    end
 
-    expect(player.to_s).to eq("I'm Larry with a health of 150 and a score of 155.")
-  end
+    it "computes a score as the sum of its health and a length of name" do
+      expect(@player.score).to eq(150 + 5)
+    end
 
-  it "computes a score as the sum of its health and a length of name" do
-    player = Player.new("larry", 150)
-
-    expect(player.score).to eq(150 + 5)
-  end
-
-  it "increases health by 15 when w00ted" do
-    initial_health = 150
-    player = Player.new("larry", initial_health)
-    player.w00t
-
-    expect(player.health).to eq(initial_health + 15)
-  end
-  
-  it "decreases health by 10 when blammed" do
-    initial_health = 150
-    player = Player.new("larry", initial_health)
-    player.blam
-
-    expect(player.health).to eq(initial_health - 10)
-  end
+    it "increases health by 15 when w00ted" do
+      @player.w00t
+      expect(@player.health).to eq(@initial_health + 15)
+    end
+    
+    it "decreases health by 10 when blammed" do
+      @player.blam
+      expect(@player.health).to eq(@initial_health - 10)
+    end
 
 end
